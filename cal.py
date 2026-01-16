e08d812ed5601e6910ed4478b6f1060c1a6bd8cd
Prashantgokak <prashantgokak500@gmail.com>
2026-01-16 20:03:02 +0530

    Add basic arithmetic functions to cal.py

---
+++ cal.py
@@ -0,0 +1,20 @@
+def add(a, b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def mul(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+
+if __name__=="__main__":
+    x=10
+    y=20
+    print("addition:",add(x,y))
+    print("subtraction:",sub(x,y))
+    print("multiplication:",mul(x,y))       
+    print("division:",div(x,y))
